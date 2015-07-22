d6dc135eba8fca0e47cf8d7dd3d73f3128bc06ad
Nic <cheng0516@gmail.com>
2015-07-22 13:11:59 +0200

    Started first person controller

--- Assets/_Scripts/Char_BasicMoveScript.cs
+++ Assets/_Scripts/Char_BasicMoveScript.cs
@@ -4,12 +4,8 @@ using System.Collections;
 public class Char_BasicMoveScript : Photon.MonoBehaviour {
 
 	public float speed = 10f;
-	
-	private float lastSynchronizationTime = 0f;
-	private float syncDelay = 0f;
-	private float syncTime = 0f;
-	private Vector3 syncStartPosition = Vector3.zero;
-	private Vector3 syncEndPosition = Vector3.zero;
+	public float jumpheight = 200;
+	private bool isJumping = true;
 
 	// Use this for initialization
 	void Start () {
@@ -18,12 +14,16 @@ public class Char_BasicMoveScript : Photon.MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
 		//Debug.Log(PhotonNetwork.networkingPeer.RoundTripTime);
 		if (photonView.isMine)
 		{
+			Camera.main.transform.parent = rigidbody.transform;
+			//Camera.main.transform.position = new Vector3(rigidbody.position.x, rigidbody.position.y, rigidbody.position.z);
 			InputMovement();
 			InputColorChange();
 		}
+
 	}
 
 	private void InputColorChange()
@@ -42,16 +42,35 @@ public class Char_BasicMoveScript : Photon.MonoBehaviour {
 
 	void InputMovement()
 	{
-		if (Input.GetKey(KeyCode.W))
-			rigidbody.MovePosition(rigidbody.position + transform.forward * speed * Time.deltaTime);
+		//if (photonView.isMine) {
+						if (Input.GetKey (KeyCode.W))
+			//transform.position += transform.forward * speed;		
+								rigidbody.MovePosition (rigidbody.position + transform.forward * speed * Time.deltaTime);
 		
-		if (Input.GetKey(KeyCode.S))
-			rigidbody.MovePosition(rigidbody.position - transform.forward * speed * Time.deltaTime);
+						if (Input.GetKey (KeyCode.S))
+			//transform.position += -transform.forward * speed;		
+								rigidbody.MovePosition (rigidbody.position - transform.forward * speed * Time.deltaTime);
 		
-		if (Input.GetKey(KeyCode.D))
-			rigidbody.MovePosition(rigidbody.position + transform.right * speed * Time.deltaTime);
+						if (Input.GetKey (KeyCode.D))
+			//transform.position += transform.right * speed;
+								rigidbody.MovePosition (rigidbody.position + transform.right * speed * Time.deltaTime);
 		
-		if (Input.GetKey(KeyCode.A))
-			rigidbody.MovePosition(rigidbody.position - transform.right * speed * Time.deltaTime);
+						if (Input.GetKey (KeyCode.A))
+			//transform.position += -transform.right * speed;
+								rigidbody.MovePosition (rigidbody.position - transform.right * speed * Time.deltaTime);
+						if (!isJumping) {
+								if (Input.GetKeyDown (KeyCode.Space)) {
+										transform.rigidbody.AddForce (0, jumpheight, 0);
+										isJumping = true;
+								}
+						}
+		//		}
 	}
+
+	void OnCollisionEnter(Collision c){
+				if (photonView.isMine) {
+						if (c.gameObject.name == "Plane")
+								isJumping = false;
+				}
+		}
 }
--- Assets/_Scripts/Level_NetworkController.cs
+++ Assets/_Scripts/Level_NetworkController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Level_NetworkController : MonoBehaviour {
 
 	public GameObject playerPrefab;
-
+	//public Camera cam;
 	// Use this for initialization
 	void Start () {
 		//PhotonNetwork.ConnectUsingSettings("0.1");
@@ -19,6 +19,15 @@ public class Level_NetworkController : MonoBehaviour {
 	{
 		// Spawn player
 		PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(1,1,0), Quaternion.identity, 0);
+		//GameObject.Find ("SamplePlayer");
+		//Debug.Log (playerPrefab.transform);
+
+		//Camera cam = transform.position (new Vector3 (1, 1, 1));
+
+		//Camera.main.transform.Translate (1,10,5);
+		//playerPrefab.AddComponent (Camera.main);
+		//Camera.main.transform.Translate (1,10,0);
+		//Camera.main.transform.LookAt (playerPrefab.transform);
 		Debug.Log("Connected to Room");
 	}
 }
