62c31783f41cf64b12e371057001a6751fa47d72
Nic <cheng0516@gmail.com>
2015-07-23 13:43:01 +0200

    Teleport projection working

--- Assets/_Scripts/Ability_Teleport.cs
+++ Assets/_Scripts/Ability_Teleport.cs
@@ -1,35 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Ability_Teleport : MonoBehaviour {
 	public float teleportDistance = 15f;
 	public GameObject projection;
+	public GameObject distanceIndicator;
+	
+	Vector3 teleportDirection;
 
 	// Use this for initialization
 
 	void Start () {
-		PhotonNetwork.Instantiate(projection.name, new Vector3(transform.position.x,transform.position.y,transform.position.z+2), Quaternion.identity, 0);
+		projection = Instantiate (projection, transform.position, Quaternion.identity) as GameObject;
+		projection.transform.GetComponent<MeshRenderer> ().enabled = false;
+		projection.transform.Find("BasicGun").GetComponent<MeshRenderer> ().enabled = false;
+		//projection.transform.GetComponentInChildren<MeshRenderer> ().enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 projectionPlacement = new Vector3 (transform.position.x, transform.position.y, transform.position.z + 2);
-		//projection.transform.position = projectionPlacement;
-		//Destroy (projection);
-		//Debug.Log (projection.transform.position);
-		Debug.Log (projectionPlacement);
-		if (Input.GetButtonDown ("Teleport")) {
-			Invoke ("Teleport", 0.2f); //Delay for teleporting
+
+
+		if (Input.GetButton ("Teleport")) {
+			projection.transform.GetComponent<MeshRenderer> ().enabled = true;
+			projection.transform.Find("BasicGun").GetComponent<MeshRenderer> ().enabled = true;
+			projection.transform.parent = transform;
+			Vector2 screenCentrePoint = new Vector2 (Screen.width / 2, Screen.height / 2);
+			Ray ray = Camera.main.ScreenPointToRay (screenCentrePoint);
+			
+			teleportDirection = new Vector3 (0, ray.direction.y , 1);
+			
+			Vector3 projectionPosition = teleportDirection * teleportDistance;
+			projection.transform.localPosition = projectionPosition;
+			if(Input.GetButtonDown ("Fire2")){
+				Debug.Log("Fire2");
+				Invoke ("Teleport", 0.2f); //Delay for teleporting
+			}
 		}
+		if (Input.GetButtonUp ("Teleport")) {
+			projection.transform.GetComponent<MeshRenderer> ().enabled = false;
+			projection.transform.Find("BasicGun").GetComponent<MeshRenderer> ().enabled = false;
+				}
 	}
 
 	void Teleport(){
 
-		//projection.transform.Translate (Vector3.forward * 5);
-		Vector2 screenCentrePoint = new Vector2 (Screen.width / 2, Screen.height / 2);
-		Ray ray = Camera.main.ScreenPointToRay (screenCentrePoint);
 
-		Vector3 teleportDirection = new Vector3 (0, ray.direction.y , 1);
+
+
 		transform.Translate (teleportDirection * teleportDistance);
 		Camera.main.transform.Translate(teleportDirection * teleportDistance);
 	}
