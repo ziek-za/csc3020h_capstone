4edb2750aef0dd95d1309e6bbef75e71be3bc0d7
ziek-za <papapapapapas@gmail.com>
2015-07-27 19:21:21 +0200

    Worked on Menu;

---
+++ Assets/Menu_CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu_CameraScript : MonoBehaviour {
+	Vector3 startPos;
+	
+	public float amplitude = 10f;
+	public float period = 5f;
+	
+	protected void Start() {
+		startPos = transform.position;
+	}
+	
+	protected void Update() {
+		float theta = Time.timeSinceLevelLoad / period;
+		float distance_x = amplitude * Mathf.Sin(theta);
+		float distance_y = amplitude * Mathf.Sin(theta + 35f);
+		transform.position = startPos + (Vector3.up * distance_y) + (Vector3.left * distance_x);
+	}
+}
