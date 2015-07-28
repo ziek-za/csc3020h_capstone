b6a0facdcb5e7f10470749042402ffccdccbbf29
Sam Alfred <sam.mike.alfred@gmail.com>
2015-07-28 22:46:15 +0200

    Added a particle effect, a time between shots and a bullet hole graphic to the gun

--- Assets/_Scripts/Char_BasicShootScript.cs
+++ Assets/_Scripts/Char_BasicShootScript.cs
@@ -1,42 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Char_BasicShootScript : MonoBehaviour {
-
-	public float timeBetweenShots = 0.3f;
+public class Char_BasicShootScript : Photon.MonoBehaviour {
 
 	private RaycastHit hit;
 	private Ray ray;
 	private float shotCooldown;	
 
-	void Start(){
-		shotCooldown = timeBetweenShots;
+	public GameObject bulletHolePrefab;
+	public ParticleSystem muzzleFlash;
+	public float timeBetweenShots = 0.33333f;
+	public float weaponAccuracy = 1f; //Higher values = more accurate
+
+	//Weapon accuracy is a public variable that can be changed in Unity
+	// 100 ~ perfect accuracy
+	// 1 ~ fairly accurate
+	// 0.01 ~ wildly inaccurate
+	private float GenerateRandomOffset(float distance){
+		int sign = -1;
+		if (Random.value > 0.5)
+			sign = 1;
+		return sign * Random.value * distance/weaponAccuracy;
 	}
 
-	void FixedUpdate(){
-		if (shotCooldown > 0f)
-			shotCooldown -= 0.02f;
+	void Start(){
 	}
 
 	void Update()
 	{
-		Vector2 screenCenterPoint = new Vector2(Screen.width/2, Screen.height/2);
-		ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+		if(Time.time >= shotCooldown && Input.GetButton("Fire1")) {
+			shotCooldown = Time.time + timeBetweenShots;
+			muzzleFlash.Play();
 
-		if(Physics.Raycast(ray, out hit, Camera.main.farClipPlane)) 
-		{
-			Debug.DrawLine(transform.position, hit.point);
-			if(Input.GetButtonDown("Fire1")) {
+			//First cast a perfectly accurate ray to get the distance to target
+			Vector2 screenCenterPoint = new Vector2(Screen.width/2, Screen.height/2);
+			ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+			Physics.Raycast(ray, out hit, Camera.main.farClipPlane);
+			
+			//Then ray cast randomly within in a circle to form a 'cone' closer targets are therefore more accurate and
+			//further targets are harder to hit
+			Vector2 screenOffset = new Vector2(screenCenterPoint.x + GenerateRandomOffset(hit.distance),
+			                                   screenCenterPoint.y + GenerateRandomOffset(hit.distance));
+			ray = Camera.main.ScreenPointToRay(screenOffset);
+			if(Physics.Raycast(ray, out hit, Camera.main.farClipPlane)) 
+			{
+				Debug.DrawLine(transform.position, hit.point, Color.red);
 				if (hit.transform.gameObject.GetComponent<Char_BasicHealthScript>()){
-					Char_BasicHealthScript hs = hit.transform.gameObject.GetComponent<Char_BasicHealthScript>();
-					hs.health -= 10;
+					DamageTarget(10);
+
 					//Quaternion hitDirection = Quaternion.FromToRotation(Vector3.forward, hit.normal);
 					//Vector3.Normalize(hitDirection);
 					hit.transform.gameObject.rigidbody.AddForce(Vector3.Normalize(ray.direction)*100f);
-					shotCooldown = timeBetweenShots;
+				} else {
+					//Create a bullet hole
+					Vector3 bulletHolePosition = hit.point + hit.normal * 0.01f;
+					Quaternion bulletHoleRotation = Quaternion.FromToRotation(-Vector3.forward, hit.normal);
+					GameObject hole = Instantiate(bulletHolePrefab, bulletHolePosition, bulletHoleRotation) as GameObject;
+					Destroy(hole,10f);
 				}
 			}
 		}
-	
+	}
+
+	[RPC] void DamageTarget(int damage){
+		Char_BasicHealthScript hs = hit.transform.gameObject.GetComponent<Char_BasicHealthScript>();
+		hs.health -= damage;
+		if (photonView.isMine)
+			photonView.RPC("DamageTarget",PhotonTargets.OthersBuffered, damage);
 	}
 }
