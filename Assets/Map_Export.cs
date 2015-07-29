bd7ded3568ffd7649d7acdda2dd4a16f7b9fd978
ziek-za <papapapapapas@gmail.com>
2015-07-30 00:26:50 +0200

    Added Export editor and script to MAP in level for ez-pz exports;

---
+++ Assets/Map_Export.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+using System.IO;
+
+public class Map_Export : MonoBehaviour {
+
+	public string LevelID;
+	public string LevelName;
+
+	private string defaultPath = "_LevelData/";
+	private string defaultJSON = "Base";
+	private string defaultSavePath = "Assets/Resources/";
+
+	// Main method to export/save all objects
+	public void Save() {
+		// Load file if possible
+		try {
+			if (LevelName.Length == 0 || LevelID.Length == 0) {
+				throw new System.ArgumentException("LevelID and LevelName required");
+			}
+			TextAsset ta = Resources.Load (defaultPath + defaultJSON) as TextAsset;
+			JSONNode jn = JSON.Parse (ta.text);
+			jn["name"] = LevelName;
+			jn["version"] = "1";
+			jn["levelData"]["name"] = LevelID;
+			StreamWriter sr = File.CreateText(defaultSavePath + defaultPath + LevelID + ".json");
+			sr.Write(jn.ToString());
+			sr.Close();
+		} catch (System.Exception e) {
+			Debug.LogException (e);
+		}
+	} 
+}
