40302d37750f1b609b63161c9f96227d266ef4be
ziek-za <papapapapapas@gmail.com>
2015-07-30 10:22:03 +0200

    Finished export for scene;

---
+++ Assets/Level_MapController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level_MapController : MonoBehaviour {
+
+	private GameObject Map;
+
+	// Use this for initialization
+	void Start () {
+		Map = GameObject.Find ("Map");
+		// Remove current LevelObjects
+		Destroy(Map.transform.GetChild (0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Map_Export.cs
+++ Assets/Map_Export.cs
@@ -5,8 +5,8 @@ using System.IO;
 
 public class Map_Export : MonoBehaviour {
 
-	public string LevelID;
-	public string LevelName;
+	public string LevelID = "";
+	public string LevelName = "";
 
 	private string defaultPath = "_LevelData/";
 	private string defaultJSON = "Base";
@@ -14,7 +14,7 @@ public class Map_Export : MonoBehaviour {
 
 	// Main method to export/save all objects
 	public void Save() {
-		// Load file if possible
+		// Load file if possibles
 		try {
 			if (LevelName.Length == 0 || LevelID.Length == 0) {
 				throw new System.ArgumentException("LevelID and LevelName required");
@@ -24,11 +24,40 @@ public class Map_Export : MonoBehaviour {
 			jn["name"] = LevelName;
 			jn["version"] = "1";
 			jn["levelData"]["name"] = LevelID;
+			// Generate children
+			JSONArray ja = new JSONArray();
+			ja = RecurseChildren(gameObject, ja);
+			jn["level_objects"] = ja;
+			// Save
 			StreamWriter sr = File.CreateText(defaultSavePath + defaultPath + LevelID + ".json");
 			sr.Write(jn.ToString());
 			sr.Close();
 		} catch (System.Exception e) {
 			Debug.LogException (e);
 		}
-	} 
+	}
+
+	private JSONArray RecurseChildren (GameObject curr_go, JSONArray ja) {
+		JSONNode jn = JSON.Parse ("{}");
+		jn ["prefab"] = curr_go.name;
+		// Position
+		jn ["position"] ["x"] = curr_go.transform.position.x.ToString();
+		jn ["position"] ["y"] = curr_go.transform.position.y.ToString();
+		jn ["position"] ["z"] = curr_go.transform.position.z.ToString();
+		// Rotation
+		jn ["rotation"] ["x"] = curr_go.transform.rotation.x.ToString();
+		jn ["rotation"] ["y"] = curr_go.transform.rotation.y.ToString();
+		jn ["rotation"] ["z"] = curr_go.transform.rotation.z.ToString();
+		if (curr_go.transform.childCount > 0) {
+			JSONArray children = new JSONArray();
+			for (int i = 0; i < curr_go.transform.childCount; i++) {
+				ja["-1"] = JSON.Parse(RecurseChildren(curr_go.transform.GetChild (i).gameObject, children).ToString());
+				return ja;
+			}
+			return ja;
+		} else {
+			ja["-1"] = jn;
+			return ja;
+		}
+	}
 }
