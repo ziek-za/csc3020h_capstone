7131ed21df4c013cd5807b277dc1d50a1b5cdf70
ziek-za <papapapapapas@gmail.com>
2015-07-30 15:36:27 +0200

    Imported game objects; error with initial velocity;

--- Assets/Level_MapController.cs
+++ Assets/Level_MapController.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using SimpleJSON;
 
 public class Level_MapController : MonoBehaviour {
 
 	private GameObject Map;
+	private JSONNode level_objects;
 
 	// Use this for initialization
 	void Start () {
@@ -13,15 +15,56 @@ public class Level_MapController : MonoBehaviour {
 		if (_MainController.ImportedMapObjectBool) {
 			Map = GameObject.Find ("Map");
 			// Remove current LevelObjects
-			Destroy (Map.transform.GetChild (0).gameObject);
+			Destroy (Map.transform.FindChild("LevelObjects").gameObject);
 			// Get level objects
-			//_MainController.MapObject["level_objects"][0];
+			//Debug.Log (_MainController.MapObject["level_objects"]);
+			level_objects = _MainController.MapObject["level_objects"][0];
+			RecurseChildren (level_objects, Map);
+		}
+	}
+
+	private void RecurseChildren(JSONNode jn, GameObject parent_go) {
+		GameObject spawned_prefab;
+		// Set orientation vectors
+		Vector3 pos = new Vector3(jn["position"]["x"].AsFloat,
+		                          jn["position"]["y"].AsFloat,
+		                          jn["position"]["z"].AsFloat);
+		Quaternion rotation = Quaternion.EulerAngles(jn["rotation"]["x"].AsFloat,
+		                               jn["rotation"]["y"].AsFloat,
+		                               jn["rotation"]["z"].AsFloat);
+		// Load resource object and instantiate
+		Debug.Log (jn ["prefab"]);
+		if (jn ["prefab"].Value == "LevelObjects") {
+			spawned_prefab = new GameObject();//Instantiate(pos, rotation);
+			spawned_prefab.name = "LevelObjects";
+			spawned_prefab.transform.position = pos;
+			spawned_prefab.transform.rotation = rotation;
+		} else {
+			GameObject prefab = Resources.Load ("_Prefabs/" + jn ["prefab"]) as GameObject;
+			spawned_prefab = Instantiate(prefab, pos, rotation) as GameObject;
+		}
+		if (spawned_prefab.rigidbody != null) {
+			spawned_prefab.rigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+			StartCoroutine(ResetRigidbody(spawned_prefab));
+		}
+		spawned_prefab.gameObject.transform.SetParent(parent_go.transform);
+
+		if (jn["children"].Count > 0) {
+			// Contains children
+			for (int i = 0; i < jn["children"].Count; i++) {
+				RecurseChildren (jn["children"][i], spawned_prefab);
+			}
 		}
 
+		if (spawned_prefab.rigidbody != null) {
+			spawned_prefab.rigidbody.constraints = RigidbodyConstraints.None;
+		}
+		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	IEnumerator ResetRigidbody(GameObject gm){
+		yield return new WaitForSeconds(1f);
+		gm.rigidbody.constraints = RigidbodyConstraints.None;
 	}
+
 }
--- Assets/Map_Export.cs
+++ Assets/Map_Export.cs
@@ -6,6 +6,7 @@ using System.IO;
 public class Map_Export : MonoBehaviour {
 
 	public string LevelID = "";
+	public string RawID = "";
 	public string LevelName = "";
 
 	private string defaultPath = "_LevelData/";
@@ -16,7 +17,7 @@ public class Map_Export : MonoBehaviour {
 	public void Save() {
 		// Load file if possibles
 		try {
-			if (LevelName.Length == 0 || LevelID.Length == 0) {
+			if (LevelName.Length == 0 || LevelID.Length == 0 || RawID.Length == 0) {
 				throw new System.ArgumentException("LevelID and LevelName required");
 			}
 			TextAsset ta = Resources.Load (defaultPath + defaultJSON) as TextAsset;
@@ -24,14 +25,17 @@ public class Map_Export : MonoBehaviour {
 			jn["name"] = LevelName;
 			jn["version"] = "1";
 			jn["levelData"]["name"] = LevelID;
+			jn["terrainRaw"]["name"] = RawID;
 			// Generate children
 			JSONArray ja = new JSONArray();
 			ja = RecurseChildren(gameObject, ja);
+			Debug.Log ("**Recursed Objects**");
 			jn["level_objects"] = ja;
 			// Save
 			StreamWriter sr = File.CreateText(defaultSavePath + defaultPath + LevelID + ".json");
 			sr.Write(jn.ToString());
 			sr.Close();
+			Debug.Log ("**Saved**");
 		} catch (System.Exception e) {
 			Debug.LogException (e);
 		}
@@ -45,9 +49,9 @@ public class Map_Export : MonoBehaviour {
 		jn ["position"] ["y"] = curr_go.transform.position.y.ToString();
 		jn ["position"] ["z"] = curr_go.transform.position.z.ToString();
 		// Rotation
-		jn ["rotation"] ["x"] = curr_go.transform.rotation.x.ToString();
-		jn ["rotation"] ["y"] = curr_go.transform.rotation.y.ToString();
-		jn ["rotation"] ["z"] = curr_go.transform.rotation.z.ToString();
+		jn ["rotation"] ["x"] = curr_go.transform.localRotation.x.ToString();
+		jn ["rotation"] ["y"] = curr_go.transform.localRotation.y.ToString();
+		jn ["rotation"] ["z"] = curr_go.transform.localRotation.z.ToString();
 		if (curr_go.transform.childCount > 0) {
 			JSONArray children = new JSONArray();
 			for (int i = 0; i < curr_go.transform.childCount; i++) {
