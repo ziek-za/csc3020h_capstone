fe365f1d2aa46c20d2e84d869acda3dcf2a982a3
Sam Alfred <sam.mike.alfred@gmail.com>
2015-09-16 17:06:21 +0200

    Started work on a turret

--- Assets/_Scripts/Ability/Ability_BuilderPlaceFoundations.cs
+++ Assets/_Scripts/Ability/Ability_BuilderPlaceFoundations.cs
@@ -28,42 +28,47 @@ public class Ability_BuilderPlaceFoundations : Photon.MonoBehaviour {
 	void linkCooledDown(){
 		linkOffCooldown = true;
 	}
+
+	void CastShadowBuildingRay(){
+		placeDistance=maxDistance;
+		
+		aimingPoint.transform.parent = transform;
+		Vector2 screenCentrePoint = new Vector2 (Screen.width / 2, Screen.height / 2);
+		Ray ray = Camera.main.ScreenPointToRay (screenCentrePoint);
+		RaycastHit hit;
+		if(Physics.Raycast (ray, out hit, Camera.main.farClipPlane)){
+			if(hit.distance<maxDistance){
+				placeDistance=hit.distance;
+			}
+		}
+		
+		
+		teleportDirection = new Vector3 (0, ray.direction.y , 1);
+		
+		Vector3 projectionPosition = teleportDirection * placeDistance;
+		projectionPosition.y += 1.5f;
+		aimingPoint.transform.localPosition = projectionPosition;
+		//projection.transform.localPosition = new Vector3(proje
+		
+		//Floor projection
+		RaycastHit floorHit;
+		Ray floorRay = new Ray(aimingPoint.transform.position, Vector3.down);
+		if(Physics.Raycast(floorRay, out floorHit, Mathf.Infinity)){
+			Debug.DrawRay(floorRay.origin, floorHit.point-floorRay.origin, Color.red);
+			distanceIndicator.transform.GetComponent<MeshRenderer> ().enabled = true;
+			Vector3 floorPosition = floorHit.point+new Vector3(0,0.5f,0)+floorHit.normal*0.01f;
+			Quaternion floorRotation = Quaternion.FromToRotation(-Vector3.forward, floorHit.normal);
+			distanceIndicator.transform.position = floorPosition;
+			distanceIndicator.transform.rotation=floorRotation;
+		}
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (photonView.isMine){
 			if (Input.GetButton("Building 1") && linkOffCooldown){
-				placeDistance=maxDistance;
-				
-				aimingPoint.transform.parent = transform;
-				Vector2 screenCentrePoint = new Vector2 (Screen.width / 2, Screen.height / 2);
-				Ray ray = Camera.main.ScreenPointToRay (screenCentrePoint);
-				RaycastHit hit;
-				if(Physics.Raycast (ray, out hit, Camera.main.farClipPlane)){
-					if(hit.distance<maxDistance){
-						placeDistance=hit.distance;
-					}
-				}
-				
-				
-				teleportDirection = new Vector3 (0, ray.direction.y , 1);
-				
-				Vector3 projectionPosition = teleportDirection * placeDistance;
-				projectionPosition.y += 1.5f;
-				aimingPoint.transform.localPosition = projectionPosition;
-				//projection.transform.localPosition = new Vector3(proje
-				
-				//Floor projection
-				RaycastHit floorHit;
-				Ray floorRay = new Ray(aimingPoint.transform.position, Vector3.down);
-				if(Physics.Raycast(floorRay, out floorHit, Mathf.Infinity)){
-					Debug.DrawRay(floorRay.origin, floorHit.point-floorRay.origin, Color.red);
-					distanceIndicator.transform.GetComponent<MeshRenderer> ().enabled = true;
-					Vector3 floorPosition = floorHit.point+new Vector3(0,0.5f,0)+floorHit.normal*0.01f;
-					Quaternion floorRotation = Quaternion.FromToRotation(-Vector3.forward, floorHit.normal);
-					distanceIndicator.transform.position = floorPosition;
-					distanceIndicator.transform.rotation=floorRotation;
-				}
+
+				CastShadowBuildingRay();
 
 				if (Input.GetButtonDown("Fire2")
 				    && transform.GetComponent<Char_AttributeScript>().energy >= placeLinkEnergyCost && linkOffCooldown){
---
+++ Assets/_Scripts/Ability/Ability_BuilderTurret.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Ability_BuilderTurret : MonoBehaviour {
+
+	public Char_AttributeScript.Teams currentTeam;
+	float lifeTime = 10000f;
+	public int health = 100;
+	public GameObject muzzle;
+	
+	float lifetimeAccum = 0;
+	
+	List<GameObject> trackedEnemies;
+
+	public GameObject leftConeEdge, rightConeEdge;
+	
+	// Use this for initialization
+	void Start () {
+		trackedEnemies = new List<GameObject>();
+	}
+	
+	public void ChangeHP(int change){
+		health += change;
+	}
+
+	float oldAngle = 0;
+	float rotationsPerSecond = 0.1f;
+	float rotationAccum = 0;
+
+	void Update(){
+		lifetimeAccum += Time.deltaTime;
+		
+		if (lifetimeAccum >= lifeTime){
+			KillSelf();
+		}
+		
+		if (health <= 0){
+			KillSelf();
+		}
+
+		rotationAccum += Time.deltaTime;
+
+		if (trackedEnemies.Count > 0){
+
+			Vector3 dir = transform.position - trackedEnemies[0].transform.position; // find direction
+			//dir.y = 0; // keep only the horizontal direction
+			Quaternion rot = Quaternion.LookRotation(dir);
+
+			//muzzle.transform.LookAt(trackedEnemies[0].transform.position);
+			//Vector3 muzzleDir = muzzle.transform.position - trackedEnemies[0].transform.position;
+			//Debug.Log(Vector3.Angle(dir,muzzleDir));
+			transform.rotation = Quaternion.Slerp(transform.rotation, rot, 5f * Time.deltaTime);
+			//Debug.Log (dir);
+		}
+	}
+	
+	public void SetTeam(Char_AttributeScript.Teams newTeam){
+		currentTeam = newTeam;
+		if (currentTeam == Char_AttributeScript.Teams.BLUE)
+			InitBlue();
+		else if (currentTeam == Char_AttributeScript.Teams.RED)
+			InitRed();
+		//Invoke("KillSelf",lifeTime);
+	}
+	
+	public void SetLifetime(float baselifetime){
+		lifetimeAccum = baselifetime;
+	}
+	
+	void InitBlue(){
+		currentTeam = Char_AttributeScript.Teams.BLUE;
+		this.GetComponent<MeshRenderer>().materials[0].color =  Color.blue;
+	}
+
+	void InitRed(){
+		currentTeam = Char_AttributeScript.Teams.RED;
+		this.GetComponent<MeshRenderer>().materials[0].color =  Color.red;
+	}
+
+	
+	void OnTriggerExit(Collider other){
+		if (other.GetComponent<Char_AttributeScript>()){// &&  other.GetComponent<Char_AttributeScript>().team != currentTeam){
+			Debug.Log("exit");
+			trackedEnemies.Remove(other.gameObject);
+		}
+	}
+	
+	void OnTriggerEnter(Collider other){
+		if (other.GetComponent<Char_AttributeScript>()){// &&  other.GetComponent<Char_AttributeScript>().team != currentTeam){
+			Debug.Log("enter");
+			trackedEnemies.Add(other.gameObject);
+		}
+	}
+
+	void KillSelf(){
+		gameObject.tag = "Untagged";
+		particleSystem.Play();
+		Invoke("ActuallyRemove",0.1f);
+	}
+	
+	void ActuallyRemove(){
+		Destroy(transform.parent.gameObject);
+	}
+}
