2ab9fc5edf1a4bde93d18491eef115c6fda00d58
Nic <cheng0516@gmail.com>
2015-10-13 10:37:03 +0200

    merge master

---
+++ Assets/_Scripts/Audio/MenuMusic.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMusic : MonoBehaviour {
+	AudioSource audio;
+	bool play=true;
+	bool sound1=false;
+	public AudioClip track1;
+	public AudioClip track2;
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (play) {
+			play=false;
+			generateNextTrack();
+			audio.Play ();
+			StartCoroutine(waitSongTime());
+		}
+	}
+
+	void generateNextTrack(){
+		if (!sound1) {
+			audio.clip=track1;
+			sound1= true;
+		}else{
+			audio.clip=track2;
+			sound1=false;
+		}
+	}
+
+	IEnumerator waitSongTime(){
+		yield return new WaitForSeconds(audio.clip.length);
+	}
+
+
+}
